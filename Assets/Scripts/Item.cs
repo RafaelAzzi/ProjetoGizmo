d70787afc32772bb86ad66a9ea10b12bb3d7e2bf
Rafael A <rafael.augusto@uscsonline.com.br>
2026-03-04 22:00:53 -0300

    .

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,9 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public enum ItemPhase 
+{
+    // --- FASE DOS ITENS --- 
+    Fase1_Comum,
+    Fase2_Raro,
+    Fase3_Lendario
+}
+
+public enum Rarity
+{
+    // --- DEFINE A RARIDADE ---
+    Comum,
+    Raro,
+    Lendario
+}
+
+public enum ItemType
+{
+    // --- FASE 1 (COMUNS) ---
+    Sucata,
+    Fiacao,
+    Bateria,
+
+    // --- FASE 2 (RAROS) ---
+    ChipComum,
+    CircuitoRemendado,
+    CelulaEnergia,
+    Potencializador,
+    SistemaEletrico,
+    PlacaCircuitos,
+
+    // --- FASE 3 (LENDÁRIOS) ---
+    UnidadeUltraLogica,
+    NucleoNanomassa,
+    ChipAprimorado,
+    ModuloAutomacao,
+    Supercontrolador,
+    TurboReator,
+    CircuitoModeloX,
+    RedeOptica,
+    ProcessadorOmega,
+    CelulaTesla,
+    MatrizIonica,
+    CoreIntraneural,
+    SistemaStorm,
+    PlacaOverdrive,
+    MalhaQuantum
+
+}
 
 public class Item : MonoBehaviour
 {
-    // Por enquanto está vazio.
-    // Não definidos os itens / obejtos.
+    [Header("Identidade do item")]
+    public ItemType itemType;
+
+    [Header("Classificação")]
+    public ItemPhase phase;
+    public Rarity rarity;
 }
