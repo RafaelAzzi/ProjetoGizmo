490c24dce42cb60de3615375c4a2021ae7f4a5a2
Rafael A <rafael.augusto@uscsonline.com.br>
2026-03-05 19:36:23 -0300

    .

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject itemPrefab; // Prefab do item que esse spawner vai criar
+    public float spawnDelay = 3f;
+    private GameObject currentItem; // Guarda referência do item que está spawnado 
+    private float timer;
+
+    void Start()
+    {
+        SpawnItem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentItem == null && currentItem.transform.parent != transform) 
+        {
+            currentItem = null; // Limpa referência
+        }
+
+        // Se não existir item, começa a contagem
+        if(currentItem = null)
+        {
+            timer += Time.deltaTime; // Faz o tempo contar corretamente
+
+            if(timer >= spawnDelay)
+            {
+                SpawnItem();
+                timer = 0f;
+            }
+        }
+    }
+    void SpawnItem()
+    {
+        // Crie o item na posição do spawner
+        currentItem = Instantiate(itemPrefab,transform.position,Quaternion.identity); 
+
+        currentItem.transform.parent = transform;
+    }
+}
